0fe3752499776a9992927c98fca37d8930afb4a1
Dan Mundy <drm.mundy@gmail.com>
2013-04-09 13:39:01 -0400

    Controls the servo based on the flex value

--- robohand.ino
+++ robohand.ino
@@ -1,14 +1,29 @@
+// Libraries
+#include <Servo.h>
+
 // Sensor locations
 #define FLEX_PIN A0
+#define SERVO_PIN 9
 
 // Sensor values
 int flexValue;
 
+// Program objects
+Servo finger;
+int position;
+
 void setup() {
     Serial.begin(9600);
+    finger.attach(SERVO_PIN);
 }
 
 void loop() {
     flexValue = analogRead(FLEX_PIN);
+    position = map(flexValue, 450, 830, 0, 179);
+    finger.write(position);
+
     Serial.println(flexValue);
+
+    //200
+    //560
 }
